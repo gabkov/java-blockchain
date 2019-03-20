456990c73ac64e928dbf29f35b9eb3fcc1c97a0c
Gábor Kovács <gabor950601@gmail.com>
2019-03-20 14:53:45 +0100

    added mineBlock method

--- src/main/java/com/gabkov/blockchain/Block.java
+++ src/main/java/com/gabkov/blockchain/Block.java
@@ -23,4 +23,14 @@ public class Block {
 
         return calculatedhash;
     }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+
 }
