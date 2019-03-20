c98e0bc0aee91cbe12424da965016ab6938e9b17
Gábor Kovács <gabor950601@gmail.com>
2019-03-20 14:26:31 +0100

    added Genesis block with 0 previous hash

--- src/com/gabkov/blockchain/Main.java
+++ src/com/gabkov/blockchain/Main.java
@@ -3,6 +3,9 @@ package com.gabkov.blockchain;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Block genesisBlock = new Block("Hi im the first block", "0");
+        System.out.println("Hash for block 1 : " + genesisBlock.hash);
+
     }
 }
