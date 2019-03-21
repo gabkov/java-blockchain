12f8a097d4e77e4b3af57465ea55466472ff47e3
Gábor Kovács <gabor950601@gmail.com>
2019-03-21 14:00:06 +0100

    removed data field, added transactions and merkleRoot fields to the class and added addTransaction method also

--- src/main/java/com/gabkov/blockchain/Block.java
+++ src/main/java/com/gabkov/blockchain/Block.java
@@ -1,25 +1,26 @@
 package com.gabkov.blockchain;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data; //our data will be a simple message.
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<>(); //our data will be a simple message.
     private long timeStamp; //as number of milliseconds since 1/1/1970.
     private int nonce;
 
     //Block Constructor.
-    public Block(String data,String previousHash ) {
-        this.data = data;
+    public Block(String previousHash ) {
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash();
     }
 
     public String calculateHash() {
-        String calculatedhash = StringUtil.applySha256(previousHash + timeStamp + nonce + data);
+        String calculatedhash = StringUtil.applySha256(previousHash + timeStamp + nonce + merkleRoot);
 
         return calculatedhash;
     }
@@ -31,6 +32,21 @@ public class Block {
             hash = calculateHash();
         }
         System.out.println("Block Mined!!! : " + hash);
+    }
+
+    //Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((previousHash != "0")) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
     }
 
 }
