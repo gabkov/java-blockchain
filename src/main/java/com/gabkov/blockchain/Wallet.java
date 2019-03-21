870b627fc4b10cb70bfde8a9f9407f2b897f5136
Gábor Kovács <gabor950601@gmail.com>
2019-03-21 10:58:08 +0100

    added some comments

--- src/main/java/com/gabkov/blockchain/Wallet.java
+++ src/main/java/com/gabkov/blockchain/Wallet.java
@@ -1,8 +1,33 @@
 package com.gabkov.blockchain;
 
 import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
     public PrivateKey privateKey;
     public PublicKey publicKey;
+
+    public Wallet(){
+        generateKeyPair();
+    }
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG"); // The name of the pseudo-random number generation (PRNG) algorithm supplied by the SUN provider.
+                                                                        // This algorithm uses SHA-1 as the foundation of the PRNG.
+                                                                        // It computes the SHA-1 hash over a true-random seed value concatenated with a 64-bit counter which is incremented by 1 for each operation.
+                                                                        // From the 160-bit SHA-1 output, only 64 bits are used.
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+            // Initialize the key generator and generate a KeyPair
+            keyGen.initialize(ecSpec, random);   //256 bytes provides an acceptable security level
+            KeyPair keyPair = keyGen.generateKeyPair();
+            // Set the public and private keys from the keyPair
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        }catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
