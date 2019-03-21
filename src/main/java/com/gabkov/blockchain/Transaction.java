ddc745e90df1313d294657dcc1fd11472b341e96
Gábor Kovács <gabor950601@gmail.com>
2019-03-21 12:05:16 +0100

    added processTransaction, getInputsValue and getOutputsValue methods

--- src/main/java/com/gabkov/blockchain/Transaction.java
+++ src/main/java/com/gabkov/blockchain/Transaction.java
@@ -39,9 +39,69 @@ public class Transaction {
         String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + value;
         signature = StringUtil.applyECDSASig(privateKey,data);
     }
+
     //Verifies the data we signed hasnt been tampered with
     public boolean verifiySignature() {
         String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + value;
         return StringUtil.verifyECDSASig(sender, data, signature);
     }
+
+    //Returns true if new transaction could be created.
+    public boolean processTransaction() {
+
+        if(verifiySignature() == false) {
+            System.out.println("#Transaction Signature failed to verify");
+            return false;
+        }
+
+        //gather transaction inputs (Make sure they are unspent):
+        for(TransactionInput i : inputs) {
+            i.UTXO = DumbChain.UTXOs.get(i.transactionOutputId);
+        }
+
+        //check if transaction is valid:
+        if(getInputsValue() < DumbChain.minimumTransaction) {
+            System.out.println("#Transaction Inputs to small: " + getInputsValue());
+            return false;
+        }
+
+        //generate transaction outputs:
+        float leftOver = getInputsValue() - value; //get value of inputs then the left over change:
+        transactionId = calulateHash();
+        outputs.add(new TransactionOutput( this.reciepient, value,transactionId)); //send value to recipient
+        outputs.add(new TransactionOutput( this.sender, leftOver,transactionId)); //send the left over 'change' back to sender
+
+        //add outputs to Unspent list
+        for(TransactionOutput o : outputs) {
+            DumbChain.UTXOs.put(o.id , o);
+        }
+
+        //remove transaction inputs from UTXO lists as spent:
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it
+            DumbChain.UTXOs.remove(i.UTXO.id);
+        }
+
+        return true;
+    }
+
+    //returns sum of inputs(UTXOs) values
+    public float getInputsValue() {
+        float total = 0;
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it
+            total += i.UTXO.value;
+        }
+        return total;
+    }
+
+    //returns sum of outputs:
+    public float getOutputsValue() {
+        float total = 0;
+        for(TransactionOutput o : outputs) {
+            total += o.value;
+        }
+        return total;
+    }
+
 }
