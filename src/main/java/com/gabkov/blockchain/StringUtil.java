87e7d6ce935dd241a0f1a09369e21940244a3916
Gábor Kovács <gabor950601@gmail.com>
2019-03-21 13:41:52 +0100

    added getMerkleRoot method to StringUtils

--- src/main/java/com/gabkov/blockchain/StringUtil.java
+++ src/main/java/com/gabkov/blockchain/StringUtil.java
@@ -1,6 +1,7 @@
 package com.gabkov.blockchain;
 
 import java.security.*;
+import java.util.ArrayList;
 import java.util.Base64;
 
 public class StringUtil {
@@ -54,5 +55,26 @@ public class StringUtil {
 
     public static String getStringFromKey(Key key) {
         return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+
+    //Tacks in array of transactions and returns a merkle root.
+    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+        int count = transactions.size();
+        ArrayList<String> previousTreeLayer = new ArrayList<>();
+        for(Transaction transaction : transactions) {
+            previousTreeLayer.add(transaction.transactionId);
+        }
+        ArrayList<String> treeLayer = previousTreeLayer;
+        while(count > 1) {
+            treeLayer = new ArrayList<>();
+            for(int i=1; i < previousTreeLayer.size(); i++) {
+                treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+            }
+            count = treeLayer.size();
+            previousTreeLayer = treeLayer;
+        }
+        String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+        return merkleRoot;
     }
 }
