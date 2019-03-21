e487049afad9c5533286e0ce1569510e22c96310
Gábor Kovács <gabor950601@gmail.com>
2019-03-21 11:46:35 +0100

    implemented TransactionOutPut with some comment

--- src/main/java/com/gabkov/blockchain/TransactionOutput.java
+++ src/main/java/com/gabkov/blockchain/TransactionOutput.java
@@ -1,4 +1,29 @@
 package com.gabkov.blockchain;
 
+import java.security.PublicKey;
+
+/**
+ * Transaction outputs will show the final amount sent to each party from the transaction.
+ * These, when referenced as inputs in new transactions, act as proof that you have coins to send.
+ */
+
 public class TransactionOutput {
+    public String id;
+    public PublicKey reciepient; //also known as the new owner of these coins.
+    public float value; //the amount of coins they own
+    public String parentTransactionId; //the id of the transaction this output was created in
+
+    //Constructor
+    public TransactionOutput(PublicKey reciepient, float value, String parentTransactionId) {
+        this.reciepient = reciepient;
+        this.value = value;
+        this.parentTransactionId = parentTransactionId;
+        this.id = StringUtil.applySha256(StringUtil.getStringFromKey(reciepient)+Float.toString(value)+parentTransactionId);
+    }
+
+    //Check if coin belongs to you
+    public boolean isMine(PublicKey publicKey) {
+        return (publicKey == reciepient);
+    }
+
 }
