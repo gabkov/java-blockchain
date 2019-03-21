f20f0d66b43a810719b93eb8cd56229fd4982a9a
Gábor Kovács <gabor950601@gmail.com>
2019-03-21 09:05:02 +0100

    added mining to DumbChain

--- src/main/java/com/gabkov/blockchain/DumbChain.java
+++ src/main/java/com/gabkov/blockchain/DumbChain.java
@@ -7,14 +7,24 @@ import java.util.ArrayList;
 public class DumbChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 5;
 
     public static void main(String[] args) {
 
         blockchain.add(new Block("Hi im the first block", "0"));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
+
         blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
+
         blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");
         System.out.println(blockchainJson);
 
     }
