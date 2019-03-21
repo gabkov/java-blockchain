d6362a373c022bff8a89b256db48cb9d208f7179
Gábor Kovács <gabor950601@gmail.com>
2019-03-21 11:44:10 +0100

    implemented TransactionInput class with some comment

--- src/main/java/com/gabkov/blockchain/TransactionInput.java
+++ src/main/java/com/gabkov/blockchain/TransactionInput.java
@@ -1,4 +1,18 @@
 package com.gabkov.blockchain;
 
+/*
+  This class will be used to reference TransactionOutputs that have not yet been spent.
+  The transactionOutputId will be used to find the relevant TransactionOutput,
+  allowing miners to check your ownership.
+*/
+
+
 public class TransactionInput {
+
+    public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+    public TransactionOutput UTXO; //Contains the Unspent transaction output
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
 }
