2f2edbb9154e456f920096a5d12b1d33800a02f3
Gábor Kovács <gabor950601@gmail.com>
2019-03-21 11:18:40 +0100

    added applyECDSASig and verifyECDSASig helper to StringUtil class

--- src/main/java/com/gabkov/blockchain/StringUtil.java
+++ src/main/java/com/gabkov/blockchain/StringUtil.java
@@ -1,6 +1,7 @@
 package com.gabkov.blockchain;
 
-import java.security.MessageDigest;
+import java.security.*;
+import java.util.Base64;
 
 public class StringUtil {
     //Applies Sha256 to a string and returns the result.
@@ -20,5 +21,38 @@ public class StringUtil {
         catch(Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //Applies ECDSA Signature and returns the result ( as bytes ).
+    public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+        Signature dsa;
+        byte[] output = new byte[0];
+        try {
+            dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            byte[] realSig = dsa.sign();
+            output = realSig;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
+
+    //Verifies a String signature
+    public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+        try {
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+            return ecdsaVerify.verify(signature);
+        }catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getStringFromKey(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 }
